4538f1d2f85e06eaffddb8d12878060781fe02e2
br-mendes <br-mendes@users.noreply.github.com>
2026-02-02 16:21:20 -0300

    fix: Remove syntax error in NotificationsRoute
    - Remove leftover comment code causing build failure
    - Clean up route component to proper import/export
    - Fix unexpected } syntax error

--- routes/NotificationsRoute.tsx
+++ routes/NotificationsRoute.tsx
@@ -1,8 +1,3 @@
 import { NotificationsPage } from '../pages/NotificationsPage';
 
 export const NotificationsRoute = NotificationsPage;
-
-  // Just return AppContent which already handles notification display
-  // The route change alone is enough to show notifications in full page
-  return <AppContent />;
-};
